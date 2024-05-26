aa1693839f2280f080b85ed6a0bff400529b9c87
bsdaoquang <bsdaoquang@gmail.com>
2024-05-26 22:09:23 +0700

    add delete api

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react';
+
+const PostItem = ({ item }) => {
+	return <div>PostItem</div>;
+};
+
+export default PostItem;
---
+++ src/components/UserItem.jsx
@@ -0,0 +1,30 @@
+/** @format */
+
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const UserItem = ({ id }) => {
+	const [user, setUser] = useState();
+
+	useEffect(() => {
+		getUserDetail();
+	}, [id]);
+
+	const getUserDetail = async () => {
+		const api = `http://localhost:3001/user/${id}`;
+
+		try {
+			const res = await axios(api);
+
+			if (res && res.data && res.status === 200) {
+				setUser(res.data.data);
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return <div>{user && <p>{user.name}</p>}</div>;
+};
+
+export default UserItem;
