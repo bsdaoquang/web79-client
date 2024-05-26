66262fdb28680295f38bfc3a3d6e0decf9fadea2
bsdaoquang <bsdaoquang@gmail.com>
2024-05-26 21:30:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { List } from 'antd';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const baseUrl = 'http://localhost:3001';
+
+const App = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		getPosts();
+	}, []);
+
+	const getPosts = async () => {
+    setIsLoading(true)
+		try {
+      const api = `${baseUrl}/posts`
+      const res = await axios(api)
+
+      if (res && res.data && res.status === 200) {
+        setPosts(res.data.data)
+      }
+      setIsLoading(false)
+		} catch (error) {
+			console.log(error);
+      setIsLoading(false)
+		}
+	};
+
+	return <div>
+    <List pagination dataSource={posts} loading={isLoading} renderItem={(item) => <List.Item key={item.id}>
+      <List.Item.Meta title={item.title} description={item.body} />
+    </List.Item>} />
+  </div>;
+};
 
 export default App;
